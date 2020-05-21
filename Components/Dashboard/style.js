3828baf40a1cd064dbe161dfade01708d36f4d08
Guruprasad BR <guruprasad.br@terralogic.com>
2020-05-21 18:06:21 +0530

    adding sidebar

--- Components/Dashboard/style.js
+++ Components/Dashboard/style.js
@@ -1,5 +1,11 @@
+import React from 'react';
+import {
+    Animated
+} from 'react-native';
+
 
 export default {
+ 
     container: {
         flex: 1,
         backgroundColor: '#fff',
@@ -7,19 +13,41 @@ export default {
         alignItems: 'center',
         justifyContent: 'flex-start',
     },
-    menuBar:{
-        // backgroundColor:"blue",
-        borderBottomStyle:"dotted",
-        borderBottomColor:"gray",
-        borderBottomWidth:1,
+    touchableContainer:{
+        flex: 1,
+        // backgroundColor: 'green',
         width:"100%",
-        height:50,
     },
-    menuBtn:{
-        position:"absolute",
-        top:10,
-        left:10,
-        color:"black",
-        fontSize:35,
+    menuBar: {
+        width: "100%",
+        height: 50,
+        backgroundColor:"blue",
+        borderBottomStyle: "dotted",
+        borderBottomColor: "gray",
+        borderBottomWidth: 1,
+    },
+    menuBtn: {
+        position: "absolute",
+        top: 10,
+        left: 10,
+        color: "black",
+        fontSize: 35,
+    },
+    sidebar: {
+        position: "absolute",
+        left: 0,
+        top: 0,
+        zIndex:100,
+        height:"100%",
+        backgroundColor:"pink",
+    },
+    sidebar_bg:{
+        backgroundColor:"violet",
+        flex: 1,
+        // width:"80%",
+    },    
+    body: {
+        backgroundColor:"yellow",
+        flex:1,
     }
 }
